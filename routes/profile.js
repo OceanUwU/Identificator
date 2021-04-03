b4f6fd1e8c562cc9738b4906f18a542b8902940c
Ocean <flynn@mos.wales>
2021-04-03 18:52:14 +0100

    finally safdsafasf

--- routes/profile.js
+++ routes/profile.js
@@ -22,25 +22,18 @@ const renderProfile = (req, res) => {
     res.render('profile', {user: global.userToSend(req.user), userFound: global.userToSend(res.locals.userFound), flagOrder: global.flagOrder});
 };
 
-var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-global.app.get('/u/:userID/', /*cors(corsOptions),*/ async (req, res, next) => {
+global.app.get('/u/:userID/', async (req, res, next) => {
     res.locals.userFound = await global.db.User.findOne({where: {id: req.params.userID}});
     next();
 }, renderProfile);
 
-global.app.get('/un/:toFind/', /*cors(corsOptions),*/ async (req, res, next) => {
+global.app.get('/un/:toFind/', async (req, res, next) => {
     res.locals.userFound = await global.findUserByUsername(req.params.toFind);
     next();
 }, renderProfile);
 
 
 const sendJSON = (req, res) => {
-    console.log('s');
-    //res.set('Access-Control-Allow-Origin', '*');
     if (res.locals.userFound == null)
         return res.json(null);
     res.json(global.userToSend(res.locals.userFound, req.query.f ? base62.decode(req.query.f) : undefined));
