d47cc4e993ee048d96017dd3ef14c3d7c1a76f53
Ocean <flynn@mos.wales>
2021-04-03 18:51:22 +0100

    aaaaaaaaaaaaaa

--- routes/profile.js
+++ routes/profile.js
@@ -19,8 +19,6 @@ global.findUserByUsername = str => {
 };
 
 const renderProfile = (req, res) => {
-    console.log('s');
-    res.set('Access-Control-Allow-Origin', '*');
     res.render('profile', {user: global.userToSend(req.user), userFound: global.userToSend(res.locals.userFound), flagOrder: global.flagOrder});
 };
 
@@ -41,17 +39,19 @@ global.app.get('/un/:toFind/', /*cors(corsOptions),*/ async (req, res, next) => 
 
 
 const sendJSON = (req, res) => {
+    console.log('s');
+    //res.set('Access-Control-Allow-Origin', '*');
     if (res.locals.userFound == null)
         return res.json(null);
     res.json(global.userToSend(res.locals.userFound, req.query.f ? base62.decode(req.query.f) : undefined));
 };
 
-global.app.get('/u/:userID/json', async (req, res, next) => {
+global.app.get('/u/:userID/json', cors(), async (req, res, next) => {
     res.locals.userFound = await global.db.User.findOne({where: {id: req.params.userID}});
     next();
 }, sendJSON);
 
-global.app.get('/un/:toFind/json', async (req, res, next) => {
+global.app.get('/un/:toFind/json', cors(), async (req, res, next) => {
     res.locals.userFound = await global.findUserByUsername(req.params.toFind);
     next();
 }, sendJSON);
